7679c0a5261623bdc73c7112d5348627da6423a2
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-03-28 01:30:24 +0200

    local storage mynotes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,13 +3,14 @@ const deleteAllNotesBtn: HTMLButtonElement = document.querySelector('.nav-button
 const saveNoteBtn: HTMLButtonElement = document.querySelector('.note-panel__buttons-save')!;
 const cancelNoteBtn: HTMLButtonElement = document.querySelector('.note-panel__buttons-cancel')!;
 
+const noteArea: HTMLDivElement = document.querySelector('.note-area')!;
 const notePanel: HTMLDivElement = document.querySelector('.note-panel')!;
 const notePanelCategory: HTMLSelectElement = document.querySelector('.note-panel__category')!;
 const notePanelTextarea: HTMLSelectElement = document.querySelector('.note-panel__text')!;
 const notePanelError: HTMLParagraphElement = document.querySelector('.note-panel__error')!;
 
-let selectedCategory: string, noteID: number = 0;
-
+let selectedCategory: string,
+	noteID: number = 0;
 
 const openNotePanel = () => {
 	notePanel.classList.remove('note-panel--hidden');
@@ -43,6 +44,7 @@ const createNote = () => {
 	const noteBody: HTMLDivElement = document.createElement('div');
 
 	newNote.classList.add('note');
+	newNote.setAttribute('data-id', `${noteID}`);
 
 	noteHeaderTitle.classList.add('note-header__title');
 	noteHeaderTitle.textContent = `${selectedCategory}`;
@@ -70,16 +72,20 @@ const createNote = () => {
 };
 
 const appendNotes = (newNote: HTMLDivElement) => {
-	const noteArea: HTMLDivElement = document.querySelector('.note-area')!;
-
-	localStorage.setItem(`note-${noteID}`, newNote.toString());
+	localStorage.setItem(`${noteID}`, newNote.outerHTML);
 	noteArea.append(newNote);
+
+	noteID++;
 };
 
 const deleteNote = (e: Event) => {
 	const currentNote = e.currentTarget as HTMLDivElement;
+	const currentNoteId: string = currentNote.dataset.id!;
 
+	localStorage.removeItem(`${currentNoteId}`);
 	currentNote.remove();
+
+	noteID--;
 };
 
 const selectValue = () => {
@@ -114,9 +120,26 @@ const deleteAllNotes = () => {
 	const allNotes: NodeListOf<HTMLDivElement> = document.querySelectorAll('.note');
 
 	allNotes.forEach(note => note.remove());
+	localStorage.clear();
+
+	noteID = 0;
+};
+
+const loadStoragedNotes = () => {
+	const storagedNotes = { ...localStorage };
+
+	for (const note in storagedNotes) {
+		noteArea.innerHTML += storagedNotes[note];
+		noteID = Number(note) + 1;
+	}
+
+	const newNote: NodeListOf<HTMLDivElement> = document.querySelectorAll('.note');
+	newNote.forEach(note => note.addEventListener('click', deleteNote));
 };
 
 addNoteBtn.addEventListener('click', openNotePanel);
 cancelNoteBtn.addEventListener('click', closeNotePanel);
 saveNoteBtn.addEventListener('click', addNote);
 deleteAllNotesBtn.addEventListener('click', deleteAllNotes);
+
+window.addEventListener('DOMContentLoaded', loadStoragedNotes);
